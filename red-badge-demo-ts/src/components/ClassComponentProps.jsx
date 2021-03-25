4720c8fc199ac51dad63e3c7adeb158b08d66364
Kelly Nauert <kellynauert@gmail.com>
2021-03-25 17:40:18 -0400

    updated

--- red-badge-demo-ts/src/components/ClassComponentProps.jsx
+++ red-badge-demo-ts/src/components/ClassComponentProps.jsx
@@ -1,2 +1,23 @@
 //Create a class component based on the functionalProps.jsx file
 //Keep the usage of destructuring props in this challenge.
+import React from 'react';
+import { Alert, Container } from 'reactstrap';
+
+class ClassComponentProps extends React.Component {
+  render() {
+    const displayAlerts = () =>
+      this.props.numbers.map((num, index) => (
+        <Alert key={index} color={num % 2 === 0 ? 'success' : 'danger'}>
+          The number is {num}{' '}
+        </Alert>
+      ));
+    return (
+      <Container>
+        <h3>From the Class Component</h3>
+        {displayAlerts()}
+      </Container>
+    );
+  }
+}
+
+export default ClassComponentProps;
